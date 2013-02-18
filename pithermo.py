e3a90945dc650d1126eb0f40e5c94804fb5f6810
Wojciech Sznapka <wojciech@sznapka.pl>
2013-02-18 01:35:09 +0100

    Added ability to handle multiple sensors

--- pithermo.py
+++ pithermo.py
@@ -4,9 +4,10 @@ from datetime import datetime, timedelta
 from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
-import appconfig
 import csv
 import gviz_api
+import yaml
+import json
 import os
 import pytz
 import sys
@@ -18,38 +19,82 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template('main.html', **collect())
+    return render_template('main.html', sensors=collect())
 
 @app.route('/history')
 def history():
     start = datetime.now() - timedelta(days=2)
-    data = [temperature for temperature in getCollection().find({'date': {'$gte': start}})]
-    data_table = gviz_api.DataTable({'date': ('datetime', 'Czas'), 'temperature': ('number', 'Temperatura')})
-    data_table.LoadData(data)
+    data = []
+    documents = []
+    headers = {'date': 'Data'}
+    for temperature in getCollection().find({'date': {'$gte': start}}):
+        if 'sensors' not in temperature:
+            continue
 
-    return data_table.ToJSon()
+        documents.append(temperature)
+        for sensor in temperature['sensors']:
+            if temperature['sensors'][sensor]['id'] not in headers:
+                headers[sensor] = temperature['sensors'][sensor]['name']
+
+    row = []
+    for key in headers:
+        row.append(headers[key])
+
+    data.append(row)
+
+    for document in documents:
+        row = [document['date'].strftime('%Y-%m-%d %H:%M')]
+        for key in headers:
+            if key != 'date':
+                row.append(document['sensors'][key]['temperature'])
+
+        data.append(row)
+
+    return json.dumps(data)
 
 def collect():
-    temp = os.popen('cat /sys/bus/w1/devices/28-*/w1_slave | tail -n1 | cut -f2 -d= | awk \'{print $1/1000}\'').read()
-    date = datetime.now()
+    sensors = yaml.load(file(os.path.dirname(__file__) + '/config.yml'))['sensors']
+    command = 'cat /sys/bus/w1/devices/%s/w1_slave | tail -n1 | cut -f2 -d= | awk \'{print $1/1000}\''
+    for address in sensors:
+        if os.path.exists('/sys/bus/w1/devices/' + address):
+            sensors[address]['temperature'] = round(float(os.popen(command % address).read()), 1)
 
-    return {
-      'temperature': round(float(temp), 1),
-      'date': date,
-      'date_literal': date.strftime(DATE_FORMAT),
-      'sensor': 'living-room'
-    }
+    return sensors
 
 def store():
-    getCollection().insert(collect())
+    getCollection().insert({
+        'date': datetime.now(),
+        'sensors': collect()
+    })
 
 def getCollection():
-    conn = MongoClient(appconfig.MONGO_HOST, appconfig.MONGO_PORT)
-    db = conn[appconfig.MONGO_COLLECTION]
-    db.authenticate(appconfig.MONGO_USER, appconfig.MONGO_PASS)
+    config = yaml.load(file(os.path.dirname(__file__) + '/config.yml'))['database']
+
+    conn = MongoClient(config['host'], config['port'])
+    db = conn[config['collection']]
+    db.authenticate(config['user'], config['pass'])
 
     return db.temperatures
 
+def migrate():
+    return
+    addresses = {'outdoor': '28-00000476ee01', 'living-room': '28-0000047632af'}
+    db = getCollection()
+    temps = {}
+    for document in db.find({'date': {'$gt': datetime(2013, 2, 17, 18, 00)}, 'id': {'$exists': True}}):
+        if document['date'] not in temps:
+            temps[document['date']] = {'date': document['date'], 'sensors': {}}
+
+        print document
+        temps[document['date']]['sensors'][addresses[document['id']]] = {
+            'id': document['id'],
+            'name': document['name'],
+            'temperature': document['temperature']
+        }
+        db.remove(document)
+
+    for date in temps:
+        db.insert(temps[date])
 
 if __name__ == "__main__" and len(sys.argv) == 1:
     app.run(host='0.0.0.0', debug=True)
